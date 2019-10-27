a4926f13eea4cfe089180baefab4b95e4f3871e2
EnriqueDoster <12348747+EnriqueDoster@users.noreply.github.com>
2019-10-27 11:44:30 -0600

    update parse_blat_psl.py

--- bin/parse_blat_psl.py
+++ bin/parse_blat_psl.py
@@ -1,16 +1,5 @@
 '''
-
-   Calculation of [Total # of Contigs], [Total Length], [Total # of trimmed Contigs], [Trimmed Length], [GC content],
-   [Min Contig Size [bp]], [Median Contig Size [bp]], [Mean Contig Size [bp]], [Max Contig Size [bp]],
-   [N50[bp] [# of Contigs]], [NG50[bp] [# of Contigs]], [N90 [bp] [# of Contigs]], [NG90 [bp] [# of Contigs]],
-   [Total # of Contigs > Average Gene Size]
-
-   This code creates an output.txt file with all of the statistics
-
-   Usage: python assembly_stats.py <assembly.fasta> <sample_ID> <minimum contig size> <estimated genome size> <average gene size>
-
-   Author: Nicolas Schmelling
-   Edited by: Enrique Doster
+   Author: Enrique Doster
 
 '''
 
@@ -19,35 +8,55 @@
 from __future__ import division
 from Bio import SeqIO
 from Bio import SearchIO
+from pprint import pprint # to help with troubleshooting by printing object attributes
 import sys
 
 
-
+# https://biopython.org/DIST/docs/api/Bio.SearchIO.BlatIO-module.html
+# Query is contig
+# Hit is AMR gene
 def parse_psl(blat_psl,sample_id):
     blat_qresult = SearchIO.parse(blat_psl, "blat-psl")
     search_dict = SearchIO.to_dict(blat_qresult)
+    # Empty dictionary for results
+    parsed_result_dict = {}
     num_contig_multiple_hits = 0
     mean_hit_counts = 0
     out_gene_alignments = open(sample_id + '_gene_alignments.csv', 'w')
+    #out_gene_alignments.write("Sample,contig,contig_start_coord,contig_end_coord,gene_header,gene_start_coord,gene_end_coord,alignment_length\n")
     out_gene_alignment_stats = open(sample_id + '_gene_alignment_stats.csv', 'w')
     # Go through list of results with the contig name as the index
-    for contig in search_dict:
-        contig_hit_object = search_dict[contig]
+    for contig in search_dict: # Loop through dictionary of blat results
+        contig_hit_object = search_dict[contig] # Loook up the contig name
         #print(contig)
         #print(len(contig_hit_object))
         if len(contig_hit_object) > 1:
             contig_alignments = set()
             num_contig_multiple_hits += 1
+            #print(contig)
+            #pprint(vars(contig_hit_object))
             # make set of all gene alignments to a each contig
-            for result in contig_hit_object:
-                contig_alignments.add(result.id)
+            for result in contig_hit_object:  # Loop through "QueryResutl" object
+                contig_alignments.add(result.id) # adds to set of HIT gene names
+                #for alignment in result: # loop through "Hit" object
+                    #print(contig)
+                    #pprint(vars(alignment))
+                    #for details in alignment:
+                        #pprint(vars(details))
+                        #print(details.hit_start)
             # check if any headers in megares and megmobile annotations are both present in the set of alignments
             if any([x in megares_annot.keys() for x in contig_alignments]) and any([x in megmobile_annot.keys() for x in contig_alignments]):
-                #print(contig)
-                #print(contig_alignments)    
-                for gene in contig_alignments:
-                    write_alignments = ('{},{},{}\n'.format(sample_id,contig, gene))
-                    out_gene_alignments.write(write_alignments)   
+                print(contig)
+                for qresult in search_dict[contig]: # Loop through "QueryResutl" object
+                    for alignment in qresult:  # loop through "Hit" object
+                        #pprint(vars(alignment))
+                        for details in alignment:
+                            #pprint(vars(details))
+                            #print(details.hit_id)
+                            alignment_length = details.query_end - details.query_start
+                            write_alignments = ('{},{},{},{},{},{},{},{}\n'.format(sample_id,contig,details.hit_start,details.hit_end,details.hit_id,details.query_start,details.query_end, alignment_length))
+                            out_gene_alignments.write(write_alignments)
+                #for gene in contig_alignments:
             contig_alignments.clear()
     print(len(search_dict)) # total number of contigs
     print(num_contig_multiple_hits) # number of contigs with multiple hits
@@ -57,8 +66,8 @@ def parse_psl(blat_psl,sample_id):
     out_gene_alignment_stats.write(write_stats_mapped)
     out_gene_alignments.close()
     out_gene_alignment_stats.close()
-    
-    
+
+
 def load_megares_annotations(file):
     ret = {}
     with open(file, 'r') as f:
@@ -70,7 +79,7 @@ def load_megares_annotations(file):
             ret.setdefault(entry[0], entry[1:])
     return ret
     # returns dictionary object with headers as the indices and the values containing all other annotation columns
-    
+
 def load_megmobile_annotations(file):
     ret = {}
     with open(file, 'r') as f:
@@ -82,7 +91,7 @@ def load_megmobile_annotations(file):
             ret.setdefault(entry[0], entry[1:])
     return ret
 
-    
+
 
 if __name__ == "__main__":
     blat_psl = sys.argv[1]
